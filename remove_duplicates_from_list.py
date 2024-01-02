b95fdc06bd937b980d9148b0441b542250cce709
B71 <love970120@gmail.com>
2024-01-02 19:03:20 +0800

    add f

--- remove_duplicates_from_list.py
+++ remove_duplicates_from_list.py
@@ -1,2 +1,8 @@
 def distinct(seq):
+    test_list = []
+    for i in seq:
+        if i not in test_list:
+            test_list.append(i)
+    return test_list
     pass
+print(distinct([1,2,3,4]))
