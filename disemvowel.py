6e1643677d07b1cb4546a6a23cf44ee7c2c3c060
B71 <love970120@gmail.com>
2024-01-02 17:13:27 +0800

    ADD aeiou

--- disemvowel.py
+++ disemvowel.py
@@ -1,2 +1,6 @@
 def disemvowel(s):
+    remove_vowels ="aeiouAEIOU"
+    result_string = ''.join(char if char not in remove_vowels else ''for char in s)
+    return result_string
     pass
+print(disemvowel("Hello World"))
