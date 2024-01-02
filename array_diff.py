05d203f965ca0b6ab76b6d9833c9e4f918eea216
B71 <love970120@gmail.com>
2024-01-02 22:35:30 +0800

    add array

--- array_diff.py
+++ array_diff.py
@@ -1,2 +1,11 @@
 def array_diff(a, b):
+    result_a =[]
+    for item in a :
+        print(a)
+        if item not in b:
+            result_a.append(item)
+    return result_a
     pass
+a= [1,2]
+b = [1]
+print(array_diff(a,b))
