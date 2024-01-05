987379827a6686482ab172906f3d58203553c8ec
B71 <love970120@gmail.com>
2024-01-05 22:28:33 +0800

    add categorize_new_member

--- categorize_new_member.py
+++ categorize_new_member.py
@@ -1,2 +1,10 @@
 def categorize_new_member(data):
+    arr_data = []
+    for age, value in data:
+        if age >= 55 and value > 7:
+            arr_data.append("Senior")
+        else:
+            arr_data.append("Open")
+    return arr_data
     pass
+print(categorize_new_member([(65, 8.0), (70, 7.5)]))
